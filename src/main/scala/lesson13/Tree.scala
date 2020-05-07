780d2c4ee6a876df30c0be41cbff6364ae88e895
drmeloy <drmeloy@gmail.com>
2020-05-07 10:26:54 -0700

    Lesson 13: implemented Tree and recursive fold method

--- src/main/scala/lesson13/Tree.scala
+++ src/main/scala/lesson13/Tree.scala
@@ -1,7 +1,17 @@
 package lesson13
 
-sealed trait Tree {
-  def fold
+sealed trait Tree[A] {
+  def fold[B](nodeFolder: CombinerFunction[B], leafFolder: ConverterFunction[A, B]): B
+
+  type CombinerFunction[C] = (C, C) => C
+  type ConverterFunction[D, E] = D => E
 }
 
+case class Node[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
+  def fold[B](nodeFolder: (B, B) => B, leafFolder: A => B): B =
+    nodeFolder(left.fold(nodeFolder, leafFolder), right.fold(nodeFolder, leafFolder))
+}
 
+case class Leaf[A](value: A) extends Tree[A] {
+  def fold[B](nodeFolder: (B, B) => B, leafFolder: A => B): B = leafFolder(value)
+}
